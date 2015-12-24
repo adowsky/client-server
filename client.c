ac3720602ad571f14cfc91c486f99c92e7f1e753
ado <segodj112@gmail.com>
2015-12-24 14:26:08 +0100

    Functions implementation.

--- client.c
+++ client.c
@@ -10,32 +10,47 @@
 
 typedef struct message message;
 
-message msg;
-int smd;
+message* msg;
+message tmp;
+int smd, csem;
+long my_usr_id;
 char user_type[11];
+unsigned int* s_area;
+sem_t* sem_clients;
 
 int make_request();
+void request_details(int* cmd, int* auth);
 void process_respond();
+void copy_request();
 
-
+int read_input(int count, char* dir){
+  int i = 0;
+  char ch;
+  fgets(dir, count, stdin);
+  if (strlen(dir) == count )
+    while((ch = getchar())!='\n'  && ch != EOF  );
+  }
 
 void sigint_handler(int i){
   //finalization
   munmap(s_area, sizeof(message));
   close(smd);
-  printf("\nClient closed successfully! \n");
+  if(csem)
+    sem_post(sem_clients);
+  printf("\nKlient zamknięty pomyślnie! \n");
   exit(0);
 }
 
 
 int main(){
+  printf("Ładowanie klienta...\n");
   signal(SIGINT, sigint_handler);
   smd = shm_open("/shm", O_RDWR, 0600);
   if(smd < 0){
     perror("SHM OPEN ERROR!");
     exit(-1);
   }
-  unsigned int* s_area = (unsigned int*) mmap(0, sizeof(message), PROT_WRITE | PROT_READ, MAP_SHARED, smd, 0 );
+  s_area = (unsigned int*) mmap(0, sizeof(message), PROT_WRITE | PROT_READ, MAP_SHARED, smd, 0 );
   if(s_area == (void*)-1){
     perror("mmap error!");
     exit(-1);
@@ -50,29 +65,36 @@ int main(){
     perror("Respond semafor error!");
     exit(-1);
   }
-  sem_t* sem_clients = sem_open("/sem_clients", O_RDWR);
+  sem_clients = sem_open("/sem_clients", O_RDWR);
   if(sem_clients == SEM_FAILED){
     perror("Clients semafor error!");
     exit(-1);
   }
   //end of initialization
-  msg = ((message*)s_area)[0];
+  msg = (void*) s_area;
+  csem = 0;
+  my_usr_id = -1;
   printf("Welcome in Hospital client!\n To get command list type 0 \n");
   int auth = 0;
+
   while(1){
+    int make = make_request(auth);
     sem_wait(sem_clients);
-    if(make_request(auth) == 0 ){
+    csem = 1;
+    if(make == 0 ){
+      copy_request();
+
       sem_post(sem_request);
-      while(msg.is_complete < 0){
+      sem_wait(sem_respond);
+      process_respond(&auth);
+      while(msg->is_complete < 0){
         sem_wait(sem_respond);
         process_respond(&auth);
       }
   }
     sem_post(sem_clients);
+    csem = 0;
   }
-
-printf("%s\n",msg.name);
-
   return 0;
 }
 /**
@@ -82,62 +104,119 @@ printf("%s\n",msg.name);
 int make_request(int *auth){
   if(!auth){  // autoryzuje do skutku
     char name[60], password[30];
-    printf("Name: ");
-    scanf("%s", &name);
-    printf("Password: ");
-    scanf("%s", &password);
-    strcpy(msg.message, name);
-    strcat(msg.message, password);
-    msg.command_type = 1;
-    return -1;
+    printf("Imię i nazwisko: ");
+    char c;
+    read_input(60, name);
+    printf("Hasło(jeśli puste wpisz \"\")[max. 30 zn]: ");
+    read_input(30, password);
+    if(strcmp(password,"\"\"") == 0)
+      strcpy(password,"");
+    strcpy(tmp.message, name);
+    strcat(tmp.message, ";");
+    strcat(tmp.message, password);
+    tmp.command_type = 1;
+    return 0;
   }
-  printf("Command number: ");
+  printf("Command: ");
   int cmd = -1;
-  scanf("%d", &cmd);
-  request_details(&cmd);  // tworzymy żądanie
+  char tab[40];
+  read_input(40,tab);
+  cmd = parse_comand(tab);
+  request_details(&cmd,auth);  // tworzymy żądanie
   if(cmd < 0 || cmd > 14){
     printf("Wrong command!");
     return -1;
   }
-    msg.command_type = cmd;
+    tmp.command_type = cmd;
   return 0;
+}
+int parse_comand(char* tab){
+  if(strcmp(tab, "help") == 0)
+    return 0;
+  else if(strcmp(tab, "whoami") == 0)
+    return 3;
+  else if(strcmp(tab, "register") == 0)
+    return 4;
+  else if(strcmp(tab, "details") == 0)
+    return 5;
+  else if(strcmp(tab, "update") == 0)
+    return 8;
+  else if(strcmp(tab, "list") == 0)
+    return 9;
+  return -1;
 }
 /**
 * Uściśla żądanie jeśli takie uśliślenie jest wymagane. Zwraca -1 jeśli żądanie jest nieobsługiwane
 * lub 0 - jeśli proces przebiegł pomyślnie.
 */
-void request_details(int* cmd){
+void request_details(int* cmd, int* auth){
   switch (*cmd) {
-    case 0 || 3 || 9: // help, whoami, list
-      msg.message = "";
+    case 1:
+      my_usr_id = -1;
+      tmp.user_id = -1;
+      printf("Ta funkcja spowodowała twoje wylogowanie!\n");
+      *auth = 0;
+      char tab[61];
+      printf("Imię i nazwisko(max. 60 zn): ");
+      read_input(61,tab);
+      strcpy(tmp.message, tab);
+      printf("Hasło(max. 60 zn): ");
+      read_input(61,tab);
+      strcat(tmp.message, ";");
+      strcat(tmp.message, tab);
+      tmp.command_type = 1;
+      break;
+    case 0:
+    case 3:
+    case 9: // help, whoami, list
+      strcpy(tmp.message, "");
       break;
     case 4: // rejestracja
-      printf("Typ użytkownika: ");
-      char tab[60];
-      scanf("%s",tab);
-      strcpy(msg.message,tab);
-      strcat(msg.message,";");
       printf("Imię i nazwisko: ");
-      scanf("%s",tab);
-      strcat(msg.message,tab);
-      strcat(msg.message,";");
-      printf("Hasło: ");
-      scanf("%s",tab);
-      strcat(msg.message, tab);
+      char name[60];
+      char ev='\n';
+      read_input(60,name);
+      strcpy(tmp.message,name);
+      strcat(tmp.message,";");
+      char conf;
+      printf("Hasło? (t\\N) ");
+      scanf(" %c",&conf);
+      if(conf == 't'){
+      printf("Podaj hasło: ");
+      read_input(60,tab);
+      strcat(tmp.message,tab);
+      }
+      strcat(tmp.message,";");
+      printf("Wiek? (t\\N) ");
+      scanf(" %c",&conf);
+      if(conf == 't'){
+      printf("Podaj wiek: ");
+      read_input(60,tab);
+      strcat(tmp.message,tab);
+      }
+      strcat(tmp.message,";");
+      printf("Wstępne rozpoznanie? (t\\N) ");
+      scanf(" %c",&conf);
+      if(conf == 't'){
+        char rozp[100];
+        printf("Podaj rozpoznanie(max 100 zn.): ");
+        read_input(100,tab);
+      strcat(tmp.message, rozp);
+    }
       break;
     case 5: // details
       if(strcmp(user_type, "lekarz") == 0){ // wysyła lekarz
         printf("Imię i nazwisko: ");
-        scanf("%s", msg.message);
+        read_input(60,tmp.message);
       }else if(strcmp(user_type, "rejestracja") == 0){  // wysyła rejestracja
         printf("Imię i nazwisko: ");
         char tab[60];
-        scanf("%s", tab);
-        strcmp(msg.message, tab);
-        strcat(msg.message, ";");
+        read_input(60,tab);
+        strcmp(tmp.message, tab);
+        strcat(tmp.message, ";");
         printf("Hasło: ");
-        scanf("%s", tab);
-        strcat(msg.message, tab);
+        read_input(60,tab);
+        strcat(tmp.message, tab);
       }else{  // wysyła pacjent
         printf("Nie masz uprawnień!\n");
         *cmd = -1;
@@ -145,20 +224,21 @@ void request_details(int* cmd){
       break;
     case 6: //delete
       printf("Imię i nazwisko: ");
-      scanf("%s", msg.message);
+
+      read_input(60,tmp.message);
       break;
-    case 7 || 10 || 11 ||12 ||13 || 14: //niewspierane
+    case 7:
+    case 10 ... 14:
       break;
     case 8: //update
       if(strcmp(user_type, "lekarz") == 0){ // wysyła lekarz
         printf("Imię i nazwisko: ");
-        char tab[60];
-        scanf("%s", tab);
-        strcpy(msg.message, tab);
-        strcat(msg.message, ";");
-        printf("Choroba: ");
-        scanf("%s", tab);
-        strcat(msg.message, tab);
+        char tab[100];
+        read_input(60,tmp.message);
+        strcat(tmp.message, ";");
+        printf("rozpoznanie: ");
+        read_input(100,tab);
+        strcat(tmp.message, tab);
       }else{  // wysyła rejestracja lub pacjent
         printf("Nie masz wystarczajacych uprawnień! \n");
         *cmd = -1;
@@ -175,27 +255,36 @@ void request_details(int* cmd){
 * Przetwarza otrzymaną odpowiedź serwera.
 */
 void process_respond(int* auth){
-  if(!*auth){ // Autoryzuje do skutku
-    if(msg.user_id != -1){
-      auth = 1;
-      strcpy(user_type, msg.message);
+  printf("%d\n",*auth);
+  if(! *auth){ // Autoryzuje do skutku
+    my_usr_id = msg->user_id;
+    if(my_usr_id != -1){
+      *auth = 1;
+      strcpy(user_type, msg->message);
+      printf("Zalogowano jako: %s\n",msg->message);
+    }else{
+      printf("SERVER: %s \n", msg->message);
     }
     return;
   }
-  switch (msg.command_type) {
+  switch (msg->command_type) {
     case -1:  // Błąd
-      printf("Problem z przetworzeniem żądania! \n");
+      printf("Problem z przetworzeniem żądania!\nSERVER: %s", msg->message);
       break;
-    case 2 || 3 || 5 || 7 || 9 || 10 || 11 || 12 ||13 ||14: //niewspierane i przesyłane jako tekst.
-      printf("%s",msg.message);
-      if(msg.is_complete == 0)
+    case 0:
+    case 2 ... 14:
+      printf("%s",msg->message);
+      if(msg->is_complete == 0)
         printf("\n");
       break;
-    case 4 || 6 || 8: // nic wykonało się po stronie serwera, ale nie ma wiadomości.
-      break;
-
     default:  //jakieś inne bugi
       printf("Wystapil problem z żądaniem lub żądanie nieobsugiwane! \n");
       break;
   }
+}
+void copy_request(){
+  msg->user_id = my_usr_id;
+  msg->command_type = tmp.command_type;
+  msg->is_complete = tmp.is_complete;
+  strcpy(msg->message,tmp.message);
 }
--- server.c
+++ server.c
@@ -1,30 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/mman.h>
+#include <time.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <signal.h>
 #include <string.h>
 #include <semaphore.h>
 #include "message.h"
+
+#define RESET "\033[0m"
+#define KRED  "\x1B[31m"
+#define KGRN  "\x1B[32m"
 /**
 zaktualizować register o prześwietlanie tablicy jesli usr_count == 100;
 */
 struct user{
   char name[60];
   char password[30];
-  char properties[3][60];
+  char properties[4][60];
   int type;
 };
 
 typedef struct message message;
 typedef struct user user;
-message msg;
+
+message *msg;
 int smd, tmp1, tmp2;
+unsigned int* s_area;
 sem_t *sem_request, *sem_respond, *sem_clients;
 user database[100];
 int usr_count;
-char* role = {"", "pacjent", "lekarz", "rejestracja"};
+char *role[] = {"", "pacjent", "lekarz", "rejestracja"};
 /**
 Przetwarza żądanie i umieszcza je w pamięci współdzielonej.
 */
@@ -44,7 +51,7 @@ void whoami();
 /**
 Obsluguje żądanie rejestracji. Doaje nowego użytkownika do bazy jeśli są spełnione odpowiednie warunki.
 */
-void register();
+void registr();
 /**
 Obssługuje żądanie details. Podaje szczegóły pacjenta podanego jako wartość w pamięci współdzielonej.
 */
@@ -77,14 +84,26 @@ void sigint_handler(int i){
   sem_unlink( "/sem_respond" );
   sem_close( sem_request );
   sem_unlink( "/sem_request" );
-  printf("\nServer closed successfully! \n");
+  printf("\nSerwer wyłączony pomyślnie! \n");
   exit(0);
 }
 
+void create_basic_users(){
+  printf("Tworzenie podstawowych użytkowników...\n");
+  strcpy(database[usr_count].name, "lekarz\0");
+  database[usr_count].type = 2;
+  strcpy(database[usr_count++].password, "lek\0");
+  strcpy(database[usr_count].name, "rejestracja\0");
+  database[usr_count].type = 3;
+  strcpy(database[usr_count++].password, "rej\0");
 
+}
 int main(){
   //init
+  printf("Uruchamianie serwera... \n");
   signal(SIGINT, sigint_handler);
+  printf("Inicjalizowanie struktur systemowych... ");;
+  fflush(stdout);
   smd = shm_open("/shm", O_RDWR | O_CREAT, 0600);
   if(smd < 0){
     perror("SHM OPEN ERROR!");
@@ -95,11 +114,12 @@ int main(){
     perror("ftruncate error!");
     exit(-1);
   }
-  unsigned int* s_area = (unsigned int*) mmap(0, sizeof(message), PROT_WRITE | PROT_READ, MAP_SHARED, smd, 0 );
+  s_area = (unsigned int*) mmap(0, sizeof(message), PROT_WRITE | PROT_READ, MAP_SHARED, smd, 0 );
   if(s_area == (void*)-1){
     perror("mmap error!");
     exit(-1);
   }
+  msg = (void*)s_area;
   sem_request = sem_open("/sem_request", O_RDWR | O_CREAT, 0660, 0);
   if(sem_request == SEM_FAILED){
     perror("Request semafor error!");
@@ -116,11 +136,15 @@ int main(){
     exit(-1);
   }
   usr_count = 0;
+  create_basic_users();
   //end of initialization
-
+  printf(KGRN "OK \n" RESET);
+  printf("Serwer uruchomiony pomyślnie!\n");
   while(1){
     sem_wait(sem_request);
+    printf("odebrano żądanie. \nPrzetwarzanie... \n");
     process_request();
+    printf("wysyłanie...\n");
     sem_post(sem_respond);
     sleep(2);
   }
@@ -129,12 +153,12 @@ int main(){
 }
 
 void process_request(){
-  if(msg.command_type < 0 || msg.command_type > 14){
-    msg.command_type = -1;
-    strcpy(msg.message, "Nieprawidłowa funkcja!");
+  if(msg->command_type < 0 || msg->command_type > 14){
+    msg->command_type = -1;
+    strcpy(msg->message, "Nieprawidłowa funkcja!");
     return;
   }
-  switch (msg.command_type) {
+  switch (msg->command_type) {
     case 0:
       commands();
       break;
@@ -145,7 +169,7 @@ void process_request(){
       whoami();
       break;
     case 4:
-      register();
+      registr();
       break;
     case 5:
       details();
@@ -164,95 +188,129 @@ void process_request(){
       break;
   }
 }
+
 void commands(){
-  strcpy(msg.message, "Dodatkowa funkcjonalność: update");
+  strcpy(msg->message, "Dodatkowa funkcjonalność: update");
 }
 int authenticate(){
-  char* password = strdup(msg.message);
+  char* password = strdup(msg->message);
   char* delim = ";";
-  char* name = strsep(&dup, delim); //password się utnie o name i zostanie pojedyńczy wpis.
+  char* name = strsep(&password, delim); //password się utnie o name i zostanie pojedyńczy wpis.
+  printf("Nazwa: %s\n Hasło: %s\n",name, password);
   int i;
-  msg.user_id = -1;
+  msg->user_id = -1;
   for(i=0; i<usr_count; i++){
-    if(strcmp(name, database[i].name) == 0 && strcmp(name, database[i].password) == 0){
-      msg.user_id = i;
+    if(strcmp(name, database[i].name) == 0 && strcmp(password, database[i].password) == 0){
+      printf("zautoryzowano\n");
+      msg->user_id = i;
       if(database[i].type == 1)
-        strcpy(msg.message, "pacjent");
+        strcpy(msg->message, "pacjent");
       else if(database[i].type == 2)
-        strcpy(msg.message, "lekarz");
-      else if(database[i].type == 1){
-        strcpy(msg.message, "rejestracja");
+        strcpy(msg->message, "lekarz");
+      else if(database[i].type == 3){
+        strcpy(msg->message, "rejestracja");
       }
       return 1;
     }
   }
-    strcpy(msg.message, "autryzacja zakończona niepowodzeniem");
+    strcpy(msg->message, "autryzacja zakończona niepowodzeniem");
     return 0;
 }
 void whoami(){
-  strcpy(msg.message, database[msg.user_id].name + " " + role[database[msg.user_id].type]);
+  snprintf(msg->message, sizeof(msg->message), "%s %s",database[msg->user_id].name, role[database[msg->user_id].type]);
 }
-void register(){
-  char* dup = strdup(msg.message);
+void registr(){
+  if(database[msg->user_id].type != 3){
+    strcpy(msg->message, "Brak uprawnień!");
+    return;
+  }
+  int new = -1;
+  if(usr_count >= 100){
+    int i;
+    for(i =0; i<100;i++){
+      if(strcmp(database[i].name,"") == 0){
+        new = i;
+        break;
+      }
+    }
+  }else{
+    new = usr_count;
+    usr_count++;
+  }
+  char* dup = strdup(msg->message);
   char* delim = ";";
   char* result = strsep(&dup, delim);
-  strcpy(database[++usr_count].name, result);
+  strcpy(database[new].name, result);
   result = strsep(&dup, delim);
-  strcpy(database[usr_count].password, result);
+  strcpy(database[new].password, result);
+  printf("Password: %s\n",result);
   result = strsep(&dup, delim);
-  strcpy(database[usr_count].properties[0], result);
+  strcpy(database[new].properties[0], result);
   result = strsep(&dup, delim);
-  strcpy(database[usr_count].properties[1], result);
-  result = strsep(&dup, delim);
-  strcpy(database[usr_count].properties[2], result);
-  msg.message = "Rejestracja zakończona pomyślnie!";
+  printf("%s\n",result);
+  strcpy(database[new].properties[1], result);
+  time_t rawtime;
+  struct tm * timeinfo;
+  time ( &rawtime );
+  timeinfo = localtime ( &rawtime );
+  strcpy(database[new].properties[2], asctime (timeinfo) );
+  database[new].type = 1;
+  strcpy(msg->message, "Rejestracja zakończona pomyślnie!");
 }
 void details(){
-  if(database[msg.user_id].type == 2){ //lekarz
+  if(database[msg->user_id].type == 2){ //lekarz
     int i;
     for(i=0;i<usr_count;i++){
-      if(strcmp(msg.message,database[i].name) == 0){
-        strcpy(msg.message, database[i].name + " " + database[i].properties[0] + " " +database[i].properties[1] + " " +database[i].properties[2]);
+      if(strcmp(msg->message,database[i].name) == 0){
+        snprintf(msg->message, sizeof(msg->message),"%s %s %s %s", database[i].name, database[i].properties[0], database[i].properties[1], database[i].properties[2]);
         return;
       }
     }
-  }else if(database[msg.user_id].type == 3){
-    char* password = strdup(msg.message);
+  }else if(database[msg->user_id].type == 3){
+    char* password = strdup(msg->message);
     char* delim = ";";
     char* name = strsep(&password, delim);
     int i;
     for(i=0;i<usr_count;i++){
       if(strcmp(name,database[i].name) == 0){
         if(strcmp(password,database[i].password) == 0){
-        strcpy(msg.message, database[i].name + " " + database[i].properties[0] + " " +database[i].properties[1] + " " +database[i].properties[2]);
+        snprintf(msg->message,sizeof(msg->message),"%s %s %s %s", database[i].name, database[i].properties[0],database[i].properties[1], database[i].properties[2]);
         return;
       }else{
-        strcpy(msg.message, "Niepopawne hasło!");
-        msg.command_type = -1;
+        strcpy(msg->message, "Niepopawne hasło!");
+        msg->command_type = -1;
       }
     }
   }
   }else{
-    strcpy(msg.message, "Niewystarczające uprawnienia!");
-    msg.command_type = -1;
+    strcpy(msg->message, "Niewystarczające uprawnienia!");
+    msg->command_type = -1;
   }
 }
 void delete(){
+  if(msg->user_id == 1){
+    strcpy(msg->message, "Brak uprawnień!");
+    return;
+  }
   int i;
   int done = 0;
   for(i=0;i<usr_count;i++){
-    if(strcmp(msg.message, database[i].name) == 0){
+    if(strcmp(msg->message, database[i].name) == 0){
       memset(&database[i], 0, sizeof(user));       // zerowanie database[i]
       done = 1;
     }
   }
   if(done)
-    strcpy(msg.message, "Usunieto pomyslnie");
+    strcpy(msg->message, "Usunieto pomyslnie");
   else
-    strcpy(msg.message, "Nie znaleziono uzytkownika");
+    strcpy(msg->message, "Nie znaleziono uzytkownika");
 }
 void update(){
-  char* dup = strdup(msg.message);
+  if(msg->user_id == 3){
+  strcpy(msg->message,"Niewystarczające uprawnienia!");
+  return;
+  }
+  char* dup = strdup(msg->message);
   char* delim = ";";
   char* result = strsep(&dup,delim);
   int i;
@@ -260,26 +318,29 @@ void update(){
   for(i=0; i<usr_count;i++){
     if(strcmp(result, database[i].name) == 0){ // znaleziono usera
       result = strsep(&dup, delim);
+    printf("%s\n",result);
       if(strcmp(result,"") != 0)  // hasło
         strcpy(database[i].password, result);
       result = strsep(&dup, delim);
+      printf("%s\n",result);
       if(strcmp(result,"") != 0) //wiek
         strcpy(database[i].properties[0], result);
-      result = strsep(&dup, delim);
-      if(strcmp(result,"") != 0)  //wstępne rozpoznanie
+      //result = strsep(&dup, delim);
+      //printf("%s\n",result);
+      if(strcmp(dup,"") != 0)  //wstępne rozpoznanie
         strcpy(database[i].properties[1], result);
       found = 1;
     }
   }
   if(found)
-    strcpy(msg.message, "Zaktualizowano pomyślnie!");
+    strcpy(msg->message, "Zaktualizowano pomyślnie!");
   else
-    strcpy(msg.message, "Nie zaktualizowano");
+    strcpy(msg->message, "Nie zaktualizowano");
 }
 void list(){
   int i;
     int field;
-  if(msg.is_complete == -1){ // czy jest to kontynuacja wysyłania
+  if(msg->is_complete == -1){ // czy jest to kontynuacja wysyłania
     i = tmp1;
     field = tmp2;
   }
@@ -287,31 +348,31 @@ void list(){
     i = 0;
     field = 0;
   }
-  strcpy(msg.message, "");
+  strcpy(msg->message, "");
   int size = 0;
 
   for(; i<usr_count; i++){
     if(field == 0){
       int len = strlen(database[i].name);
       if(size+len+1 >300){
-        message.is_complete = -1;
+        msg->is_complete = -1;
         tmp1 = i;
         tmp2 = field;
       }else{
-        strcat(msg.message, database[i].name);
-        strcat(msg.message, ":");
+        strcat(msg->message, database[i].name);
+        strcat(msg->message, ":");
         size+=len+1;
         field++;
       }
     }else{
       int len = strlen(role[database[i].type]);
       if(size+len+1 >300){
-        message.is_complete = -1;
+        msg->is_complete = -1;
         tmp1 = i;
         tmp2 = field;
       }else{
-        strcat(msg.message, role[database[i].type]);
-        strcat(msg.message, ":");
+        strcat(msg->message, role[database[i].type]);
+        strcat(msg->message, ":");
         field = 0;
         size+=len+1;
       }
@@ -320,5 +381,5 @@ void list(){
 
 }
 void unsupported(){
-  strcpy(msg.message, "Polecenie niewspierane");
+  strcpy(msg->message, "Polecenie niewspierane");
 }
